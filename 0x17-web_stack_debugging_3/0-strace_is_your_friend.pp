3646d153e399f36c5810d1fea2f1777b43ef2f84
Brahimovith <brahim.sabri.contact@gmail.com>
2024-04-11 17:54:44 -0700

    j

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
-exec { 'fix-apache-error':
-        command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-        provider => 'shell'
+# automated puppet fix (to find out why Apache is returning a 500 error)
+
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
